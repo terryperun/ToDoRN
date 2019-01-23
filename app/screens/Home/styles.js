ffb28224a90f585b41f62e80b7eb78818f40d642
Pikser <32457085+Pikser@users.noreply.github.com>
2019-01-23 13:47:08 +0200

    styles component

--- app/screens/Home/styles.js
+++ app/screens/Home/styles.js
@@ -1,6 +1,38 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    // backgroundColor: 'red',
+    // flex: 1,
+  },
+  containerInput: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderBottomWidth: 1.5,
+    borderColor: '#d6d7da',
+  },
+  textInput: {
+    height: 60,
+    paddingLeft: 15,
+    flex: 1,
+  },
+  icon: {
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  task: {},
+  activityIndicator: {
+    paddingRight: 5,
+  },
+  touchableBtn: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 60,
+    borderBottomWidth: 1.5,
+    borderColor: '#d6d7da',
+  },
+  touchableBtnText: {
+    color: '#7595C9',
+  },
 });
 export default styles;
