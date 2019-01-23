ff9295363771f223621c35aa8b899c8319577d72
Pikser <32457085+Pikser@users.noreply.github.com>
2019-01-23 16:43:11 +0200

    add Api

---
+++ app/api/Api.js
@@ -0,0 +1,135 @@
+import { AsyncStore } from 'react-native';
+import uuid from 'uuid/v4';
+
+const methods = {
+  post: 'post',
+  get: 'get',
+  patch: 'patch',
+  delete: 'delete',
+};
+
+function getRandomArbitrary(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+const randomDelay = () =>
+  new Promise((res) => {
+    const delay = getRandomArbitrary(200, 2000);
+
+    setTimeout(res, delay);
+  });
+
+const getId = (endpoint) => endpoint.split('/')[1];
+
+const handleRequest = async (endpoint, options, cache) => {
+  switch (options.method) {
+    case methods.get: {
+      return cache;
+    }
+
+    case methods.post: {
+      if (endpoint === 'todos/remove') {
+        const newCache = cache.filter(
+          (todo) => !options.body.includes(todo.id),
+        );
+
+        await AsyncStore.setItem('cache', newCache);
+
+        return { success: true };
+      }
+
+      const timestamp = new Date().getTime();
+      const newTodo = {
+        ...options.body,
+        id: uuid(),
+        createdAt: timestamp,
+        updatedAt: timestamp,
+      };
+      const newCache = cache.concat(newTodo);
+
+      await AsyncStore.setItem('cache', newCache);
+
+      return newTodo;
+    }
+
+    case methods.patch: {
+      const id = getId(endpoint);
+
+      let newTodo;
+
+      const newCache = cache.map((todo) => {
+        if (todo.id !== id) {
+          return todo;
+        }
+
+        newTodo = {
+          ...todo,
+          ...options.body,
+          updatedAt: new Date().getTime(),
+        };
+
+        return newTodo;
+      });
+
+      await AsyncStore.setItem('cache', newCache);
+
+      return newTodo;
+    }
+
+    case methods.delete: {
+      const id = getId(endpoint);
+
+      const newCache = cache.filter((todo) => todo.id !== id);
+
+      await AsyncStore.setItem('cache', newCache);
+
+      return { success: true };
+    }
+
+    default:
+      return cache;
+  }
+};
+
+const fetchData = async (endpoint, options = {}) => {
+  if (!options.method) {
+    options.method = methods.get; // eslint-disable-line no-param-reassign
+  }
+
+  // getting cached data from store
+  let cache = AsyncStore.getItem('cache');
+
+  if (!cache) {
+    cache = [];
+  }
+
+  const res = await handleRequest(endpoint, options, cache);
+
+  await randomDelay();
+
+  return res;
+};
+
+const TodoApi = {
+  add(body) {
+    return fetchData('todos', { method: 'post', body });
+  },
+
+  remove(id) {
+    return fetchData(`todos/${id}`, { method: 'delete' });
+  },
+
+  removeMany(ids) {
+    return fetchData('todos/remove', { method: 'post', body: ids });
+  },
+
+  getAll() {
+    return fetchData('todos');
+  },
+
+  update(id, body) {
+    return fetchData(`todos/${id}`, { method: 'patch', body });
+  },
+};
+
+export default TodoApi;
