cfade835c513c5f8795480e72a7894be92b353b2
Pikser <32457085+Pikser@users.noreply.github.com>
2019-02-06 08:23:32 +0200

    add persist

--- app/store.js
+++ app/store.js
@@ -1,12 +1,23 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'remote-redux-devtools';
+import { persistStore, persistCombineReducers } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import appReducer from './modules';
 
+const config = {
+  key: 'root',
+  storage,
+};
+
+const reducer = persistCombineReducers(config, appReducer);
+
 const store = createStore(
-  appReducer,
+  reducer,
   composeWithDevTools(applyMiddleware(thunk)),
 );
 
-export default store;
+const persistor = persistStore(store);
+
+export { store, persistor };
